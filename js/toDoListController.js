cd4cd39adb8031e84ae3d11e168bc75c2008d870
sanjsanj <sanjsanj@hotmail.com>
2015-05-02 19:26:38 +0100

    Add tests for item status toggle, edit and delete

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -1,9 +1,20 @@
 toDoList.controller('ToDoListController', [function(){
   var self = this;
+
   self.listActive = [];
+  self.listCompleted = [];
 
   self.addItem = function(item){
     self.listActive.push(item);
   };
 
+  self.deleteItem = function(item){
+    if (self.listActive.includes(item)) {
+      var index = self.listActive.indexOf(item)
+      self.listActive.splice(index, 1);
+    } else {
+      var index = self.listCompleted.indexOf(item);
+      self.listCompleted.splice(index, 1);
+    };
+  };
 }]);
--- test/toDoList.spec.js
+++ test/toDoList.spec.js
@@ -8,22 +8,54 @@ describe('ToDoListController', function() {
     ctrl = $controller('ToDoListController');
   }));
 
-  it('initialises with an empty list', function() {
+  it('initialises with an empty active list', function() {
     expect(ctrl.listActive).toEqual([]);
   });
 
+  it('initialises with an empty completed list', function() {
+    expect(ctrl.listCompleted).toEqual([]);
+  });
+
   describe('When adding items', function(){
-    var items = ["Get milk", "Wash car"];
+
+    it('it knows item is active', function(){
+      ctrl.addItem("Buy milk");
+      expect(ctrl.listActive).toContain("Buy milk");
+    });
 
     it('it can display 1 item', function(){
-      ctrl.addItem("Get milk");
-      expect(ctrl.listActive).toEqual(["Get milk"]);
+      ctrl.addItem("Buy milk");
+      expect(ctrl.listActive).toEqual(["Buy milk"]);
     });
 
     it('it can display multiple items', function(){
-      ctrl.addItem("Get milk");
-      ctrl.addItem("Wash car");
-      expect(ctrl.listActive).toEqual(["Get milk", "Wash car"]);
+      ctrl.addItem("Buy milk");
+      ctrl.addItem("Walk dog");
+      expect(ctrl.listActive).toEqual(["Buy milk", "Walk dog"]);
+    });
+
+    xit('it can edit an active item', function(){
+
+    });
+
+    it('it can delete an active item', function(){
+      ctrl.addItem("Buy milk");
+      ctrl.deleteItem("Buy milk");
+      expect(ctrl.listActive).toEqual([]);
+    });
+
+    xit('it can mark an item as completed', function(){
+
+    });
+
+    xit('it can edit a completed item', function(){
+
+    });
+
+    xit('it can delete a completed item', function(){
+      ctrl.addItem("Buy milk");
+      ctrl.deleteItem("Buy milk");
+      expect(ctrl.listActive).toEqual([]);
     });
   });
 
