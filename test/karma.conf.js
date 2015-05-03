df308c931363026cbf2549a3da2688bb06ddfd52
sanjsanj <sanjsanj@hotmail.com>
2015-05-03 13:23:05 +0100

    Add chrome_travis_ci to browsers in karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Fri May 01 2015 14:03:06 GMT+0100 (BST)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../',
@@ -21,7 +21,6 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'js/**/*.js',
       'test/**/*.spec.js',
-      'index.html'
     ],
 
 
@@ -63,9 +62,27 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+    singleRun: false
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+    // configuration.reporters = configuration.reporters.concat(['coverage', 'coveralls']);
+    // configuration.coverageReporter = {
+    //   type : 'lcovonly',
+    //   dir : 'coverage/'
+    // };
+  }
+
+  config.set(configuration);
 };
